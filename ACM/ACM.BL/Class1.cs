8b13a9a2e37af0edcf44718fe2306b00f633078c
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2020-04-10 18:22:50 +0530

    Creating the Acme Customer Management Business Layer

---
+++ ACM/ACM.BL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Class1
+    {
+    }
+}
